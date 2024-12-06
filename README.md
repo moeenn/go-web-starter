3da7059fe63f8c160702117a58bbd52b782b03a9
moeenn <moeen.v8@gmail.com>
2024-12-06 22:04:12 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Go + Templ + Tailwind
+
+Basic setup for Golang Std server, Templ and Tailwind.
+
+
+#### Setup
+
+```bash
+$ npm run setup
+```
--- scripts/tools.sh
+++ scripts/tools.sh
@@ -3,3 +3,5 @@
 # installation commands for all tool binaries goes here
 go install github.com/a-h/templ/cmd/templ@latest
 
+# dependency installation commands go here
+go mod tidy
