aa6337e349f4c07178c88387e76774e8a336d825
moeenn <moeen.v8@gmail.com>
2025-07-17 00:29:06 +0500

    dashboard table in-progress

--- public/js/spa-nav.js
+++ public/js/spa-nav.js
@@ -1,62 +1,62 @@
 // @ts-check
 
-document.addEventListener("DOMContentLoaded", main)
+document.addEventListener("DOMContentLoaded", main);
 function main() {
-    const anchors = document.querySelectorAll("a")
-    if (anchors.length == 0) return
+    const anchors = document.querySelectorAll("a");
+    if (anchors.length == 0) return;
 
     for (const anchor of anchors) {
-        if (hasHTMXAttributes(anchor)) continue
+        if (hasHTMXAttributes(anchor)) continue;
 
         anchor.addEventListener("click", async (e) => {
             if (e.detail["htmxIgnore"]) {
-                return
+                return;
             }
 
-            e.preventDefault()
-            let content = ""
-            let href = ""
+            e.preventDefault();
+            let content = "";
+            let href = "";
 
             try {
                 const anchor = /** @type {HTMLAnchorElement | undefined} */ (
                     e.currentTarget
-                )
+                );
                 if (!anchor) {
-                    throw new Error("failed to acquire anchor")
+                    throw new Error("failed to acquire anchor");
                 }
 
-                href = anchor.href
-                const res = await fetch(href)
-                content = await res.text()
+                href = anchor.href;
+                const res = await fetch(href);
+                content = await res.text();
             } catch (err) {
-                console.warn("failed to highjack event", err)
+                console.warn("failed to highjack event", err);
                 e.target?.dispatchEvent(
                     new CustomEvent("click", { detail: { htmxIgnore: true } }),
-                )
-                return
+                );
+                return;
             }
 
-            setPageContent(content)
-            window.history.pushState(content, "", href)
-            document.dispatchEvent(new Event("DOMContentLoaded"))
-        })
+            setPageContent(content);
+            window.history.pushState(content, "", href);
+            document.dispatchEvent(new Event("DOMContentLoaded"));
+        });
     }
 
     // make the back button work properly.
     window.addEventListener("popstate", (e) => {
-        if (!e.state) return
-        setPageContent(e.state)
-    })
+        if (!e.state) return;
+        setPageContent(e.state);
+    });
 }
 
 /**
  * @param {string} state
  */
 function setPageContent(state) {
-    const parser = new DOMParser()
-    const dom = parser.parseFromString(state, "text/html")
-    document.body = dom.body
-    document.title = dom.title
+    const parser = new DOMParser();
+    const dom = parser.parseFromString(state, "text/html");
+    document.body = dom.body;
+    document.title = dom.title;
 }
 
 /**
@@ -66,8 +66,8 @@ function setPageContent(state) {
 function hasHTMXAttributes(element) {
     for (const attribute of element.attributes) {
         if (attribute.name.startsWith("hx-")) {
-            return true
+            return true;
         }
     }
-    return false
+    return false;
 }
